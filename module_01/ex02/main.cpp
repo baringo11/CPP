c0061f83f4749e0f5bd0279b1c043c3d1b358f3c
javi <javi.baringo@gmail.com>
2021-10-09 23:08:14 +0200

    ex02 donde

---
+++ module_01/ex02/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/09 13:34:28 by jbaringo          #+#    #+#             */
+/*   Updated: 2021/10/09 23:07:25 by jbaringo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <string>
+
+int	main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+	
+	std::cout << "String address with '&':\t\t" << &str << std::endl;
+	std::cout << "String address with a pointer:\t" << stringPTR << std::endl;
+	std::cout << "String address with a reference:\t" << &stringREF << std::endl;
+	std::cout << "String content with a pointer:\t" << *stringPTR << std::endl;
+	std::cout << "String content with a reference:\t" << stringREF << std::endl;
+}
