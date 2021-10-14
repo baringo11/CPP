e6db027dc06f7a7163c6129f993eb19b35477aff
Javier Baringo romero <jbaringo@c2r17s3.42madrid.com>
2021-10-14 14:07:44 +0200

    align output

--- module_01/ex02/main.cpp
+++ module_01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/09 13:34:28 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/10/09 23:07:25 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/10/14 14:06:19 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,8 +20,8 @@ int	main()
 	std::string &stringREF = str;
 	
 	std::cout << "String address with '&':\t\t" << &str << std::endl;
-	std::cout << "String address with a pointer:\t" << stringPTR << std::endl;
+	std::cout << "String address with a pointer:\t\t" << stringPTR << std::endl;
 	std::cout << "String address with a reference:\t" << &stringREF << std::endl;
-	std::cout << "String content with a pointer:\t" << *stringPTR << std::endl;
+	std::cout << "String content with a pointer:\t\t" << *stringPTR << std::endl;
 	std::cout << "String content with a reference:\t" << stringREF << std::endl;
 }
