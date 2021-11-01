944c7efca83191b4542257670973e8b076a19f32
javi <javi.baringo@gmail.com>
2021-11-01 20:16:39 +0100

    \n in main

--- module_03/ex01/srcs/main.cpp
+++ module_03/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/15 20:28:13 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/10/15 22:44:43 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/11/01 20:05:46 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,13 @@ int	main(void)
 	maquina.takeDamage(8);
 	maquina.beRepaired(5);
 
+	std::cout << "\n---------------\n\n";
+
 	ScavTrap mastodonte("siri");
 	mastodonte.attack("alexa");
 	mastodonte.takeDamage(55);
 	mastodonte.beRepaired(42);
 	mastodonte.guardGate();
+
+	std::cout << "\n---------------\n\n";
 }
