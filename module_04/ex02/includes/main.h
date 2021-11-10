03fdc1a5e0b199106940cd3904a8e5f7fb6676c1
Javier Baringo romero <jbaringo@c2r18s5.42madrid.com>
2021-11-10 14:09:35 +0100

    abstract class fixed

--- module_04/ex02/includes/main.h
+++ module_04/ex02/includes/main.h
@@ -6,7 +6,7 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 19:10:30 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/10/18 20:10:52 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/11/10 13:57:05 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 
 # include <iostream>
 # include <string>
-# include "../srcs/class/Animal.hpp"
+# include "../srcs/class/AAnimal.hpp"
 # include "../srcs/class/Dog.hpp"
 # include "../srcs/class/Cat.hpp"
 # include "../srcs/class/Brain.hpp"
--- module_04/ex02/srcs/class/Dog.hpp
+++ module_04/ex02/srcs/class/Dog.hpp
@@ -6,17 +6,17 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/17 19:10:08 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/10/18 20:16:16 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/11/10 13:57:39 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef DOG_HPP
 # define DOG_HPP
 
-# include "Animal.hpp"
+# include "AAnimal.hpp"
 # include "Brain.hpp"
 
-class Dog : public Animal
+class Dog : public AAnimal
 {
 	public:
 
