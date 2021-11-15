10e1db8c0f0de043f121350caa562d4cd1d2eed1
Javier Baringo romero <jbaringo@c2r13s6.42madrid.com>
2021-11-15 14:01:12 +0100

    module08

---
+++ module_08/ex00/easyfind.hpp
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/13 20:00:58 by jbaringo          #+#    #+#             */
+/*   Updated: 2021/11/15 13:59:01 by jbaringo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+# define EASYFIND_HPP
+
+template< typename T >
+void	easyfind(T *array, int len)
+{
+	
+}
+
+#endif
