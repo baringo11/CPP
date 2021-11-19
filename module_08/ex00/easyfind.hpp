ad7e22352a97310a7e9d4aa439030a5d034f47f4
Javier Baringo romero <jbaringo@c3r13s1.42madrid.com>
2021-11-19 12:30:51 +0100

    ex00 done

--- module_08/ex00/easyfind.hpp
+++ module_08/ex00/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 20:00:58 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/11/16 12:11:29 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/11/19 12:29:30 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,10 @@ template< typename T >
 typename T::iterator	easyfind(T container, int x)
 {
 	typename T::iterator it;
-	//typename T::iterator it2 = container.end();
 
     it = find(container.begin(), container.end(), x);
-
-	std::cout << "address: " << &it << " and value: " << *it << std::endl;
-	//std::cout << "address: " << &it2 << " and value: " << *it2 << std::endl;
-
 	if (it == container.end())
-		throw std::bad_exception();
+		throw ("Value not found");
 	return (it);
 }
 
--- module_08/ex00/main.cpp
+++ module_08/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jbaringo <jbaringo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/13 20:00:36 by jbaringo          #+#    #+#             */
-/*   Updated: 2021/11/16 12:12:07 by jbaringo         ###   ########.fr       */
+/*   Updated: 2021/11/19 12:28:50 by jbaringo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,24 +19,30 @@
 int	main( void )
 {
 	int myints[] = {16,2,77,29};
-	int myints2[] = {16,2,77,29};
-	std::vector<int>	v1 (myints, myints + sizeof(myints) / sizeof(int) );
-	std::list<int>		l1 (myints2, myints2 + sizeof(myints2) / sizeof(int) );
+	std::vector<int>	v;
+	std::list<int>		l;
 	std::vector<int>::iterator	it;
 	std::list<int>::iterator	it2;
-
+	
+	for (int i = 0; i < 4; i++)
+	{
+		v.push_back(myints[i]);
+		l.push_back(myints[i]);
+	}
 	try
 	{
-		it = easyfind(v1, 77);
-		std::cout << "Vector iterator: address: " << &it << " and value: " << *it << std::endl;
-		
-		it2 = easyfind(l1, 29);
+		it = easyfind(v, 2);
+		std::cout << "Vector iterator: Found value: address: " << &it << " and value: " << *it << std::endl;
+
+		it2 = easyfind(l, 29);
+		std::cout << "List iterator:   Found value: address: " << &it2 << " and value: " << *it2 << std::endl;
+
 		std::cout << "\n-----------------\n" << std::endl;
-		std::cout << "Vector iterator: address: " << &it << " and value: " << *it << std::endl;
-		std::cout << "List iterator  : address: " << &it2 << " and value: " << *it2 << std::endl;
+
+		it = easyfind(v, 7734);
 	}
-	catch(const std::exception& e)
+	catch(const char* e)
 	{
-		std::cerr << e.what() << '\n';
+		std::cerr << e << '\n';
 	}
 }
